2356359065fdb20441156996fb257d146703525a
GWANHUN <tksxk6686@naver.com>
2021-11-10 21:28:00 +0900

    첫 페이지 수정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { type } from 'os';
 
 function App() {
+  
   return (
     <div className="App" className="App-header" >
       
     
     
-    <img src="kimleejung_logo.png" className="App-size"></img>
+    <img src="kimleejung_letterlogo.png" className="App-size"></img>
     
-    <img src="kimleejung_minilogo.png" className="App-logo" input type="text"></img>
-
+    <img src="kimleejung_minilogo.png" className="App-logo">
+    
+    
+    </img>
+    
+    
 
     <br/><br/><br/><br/><br/><br/>
     
