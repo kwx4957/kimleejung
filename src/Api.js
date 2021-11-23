4cca1c20f72403bddf2e4a73977b6279dc86827d
dansam <33277725+kwx4957@users.noreply.github.com>
2021-11-23 22:07:32 +0900

    add api.js

---
+++ src/Api.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Users() {
+  const [apis, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        // 요청이 시작 할 때 error 와 users를 null설정
+        setError(null);
+        setUsers(null);
+        // loading을  true로 설정
+        setLoading(true);
+        const response = await axios.get('http://apis.data.go.kr/1160100/service/GetStocDiviInfoService/getDiviInfo',{
+          params: {
+            serviceKey: 'H8M+zTGzEGjkWijFKjq4wni6P6SKhzbEalrHnkrdexbFlgkuWgDMviKNbCN9tqt2I8tTF5dEedbjV3QMHIkqxA==',
+            pageNo : 1,
+            numOfRows: 10,
+            resultType: 'json',
+            basDt: null,
+            crno: 1101110057012,
+            stckIssuCmpyNm: '동남합성'
+          },
+        });        
+        setUsers(response);
+        console.log(response)
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!apis) return null;
+  return true;
+
+}
+
+export default Users;
