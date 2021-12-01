0664340a46fa3441c95f581f851fe6fe2257d867
unknown <hanmajsh1@gmail.com>
2021-12-01 17:35:11 +0900

    확인

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {useState} from 'react'
 function App() {
 
   const onAlert = () => {
-    alert('다음페이지로 넘어가야 함!');
+    alert("");
   }
 
   const onKeyUp = (event) => {
@@ -22,34 +22,36 @@ function App() {
         <Api />
 
     <a>
-    {/*로고이미지*/}
-    <img src="kimleejung_minilogo.png" className="App-logo"></img>
-    {/*로고명*/}
-    <img src="kimleejung_letterlogo.png" className="App-size"></img>
+      {/*로고이미지*/}
+      <img src="kimleejung_minilogo.png" className="App-logo"></img>
+      {/*로고명*/}
+      <img src="kimleejung_letterlogo.png" className="App-size"></img>
     </a>
 
     &nbsp;&nbsp;
     {/*검색창*/}
     <b>
-    <input onKeyUp={onKeyUp} type="search" className="App-Search" placeholder="  기업명을 입력하시오." name="Enterprise" onChange={event => {setSearchTerm(event.target.value)}}/>
-    
-    {/*검색 기능*/}
-    {JSONDATA.filter((val)=> {
-      if (searchTerm == ""){
-        return null
-      } else if (val.company.toLowerCase().includes(searchTerm.toLowerCase())){
-        return val
-      }
-    }).map((val,key)=> {
-      return (
-      <div className="user" key={key}> 
-        <p> {val.company} </p>
-      </div>
-      );
-    })}
-&nbsp;
-<img src="button.png" className="App-button" onClick={onAlert}></img>
-</b>
+      <input onKeyUp={onKeyUp} type="search" className="App-Search" placeholder="  기업명을 입력하시오." id="Enterprise" onChange={event => {setSearchTerm(event.target.value)}}/>
+
+      {/*검색 기능*/}
+      {JSONDATA.filter((val)=> {
+        if (searchTerm == ""){
+          return val
+        } else if (val.company.toLowerCase().includes(searchTerm.toLowerCase())){
+          return val
+        }
+      }).map((val,key)=> {
+        return (
+        <div className="user" key={key}> 
+          <p> {val.company} </p>
+        </div>
+        );
+      })}
+      
+      &nbsp;
+      {/*검색버튼*/}
+      <img src="button.png" className="App-button" onClick={onAlert}></img>
+    </b>
 
     <br/><br/><br/><br/><br/><br/>
     
