e5cfce1fc8a17cc051995904fef3fc7e98d54104
GWANHUN <tksxk6686@naver.com>
2021-12-09 13:24:56 +0900

    Dividend 초기 디자인

--- src/Dividend.jsx
+++ src/Dividend.jsx
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './dividend.css';
 import { type } from 'os';
 import Api from './Api';
 
 
 function Dividend() {
-  
   return (
+    
     <div>
-    <a href="#tag1">
-    {/*로고이미지*/}
-    <img src="kimleejung_minilogo.png" className="App-logo"></img>
-    {/*로고명*/}
-    <img src="kimleejung_letterlogo.png" className="App-size"></img>
-    </a>
-
-    
-    
-
-    <br/><br/><br/><br/><br/><br/>
+      {/* 로고 위치 및 크기 지정*/}
+      <img src="kimleejung_logo.png"  className="divi-logo"></img>
     </div>
+
   );
 }
 
