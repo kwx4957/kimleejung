355b5553462eb6d347a7a6b4b642fff2f09efd1c
unknown <hanmajsh1@gmail.com>
2022-01-10 11:01:54 +0900

    로그인창

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,19 +1,28 @@
 import './login.css';
 import React from 'react';
 function Login () {
+    
     return (
-        
         <>
+        {/* <div className="App App-header">
+            <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/dividend" element={<Dividend/>} />
+            <Route path="/login" element={<Login/>} />
+            </Routes>
+        </div> */}
             {/* 로고 위치 및 크기 지정 */}
             <div style={{ width: '100vw' }}> 
                 <img src="kimleejung_logo.png"  className="divi-logo" alt='logo'/>
             </div>
-            <div>
-                아이디 : <input type="search" placeholder="ID"/><br/>
-                <input type="search" placeholder="PW"/>
+            <div className='sing-in'>
+                Sing In
+                <input type="search" placeholder="ID" id="id"/><br/>
+                <input type="search" placeholder="PW" id="password"/><br/>
+                <button>로그인</button>
+                <button>회원가입</button>  
             </div>
         </>
     );
-
 } 
 export default Login;
