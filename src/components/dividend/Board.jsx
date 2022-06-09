b08fcfcf173f2e5647f8ce6240542f2461e13b2e
sourcem249 <sourcem249@gmail.com>
2022-06-10 01:37:22 +0900

    게시판 리스트 한척

--- src/components/dividend/Board.jsx
+++ src/components/dividend/Board.jsx
@@ -1,20 +1,51 @@
 import { useNavigate } from "react-router-dom";
 import './Borad.css';
 
+
 const Board = () => {
     const navigate = useNavigate();
 
     const onPost = (e) => {
         e.preventDefault();
-        navigate('/post');
+        navigate('/Post');
     }
     
     return (
         <>
+            
             {/* 로고 위치 및 크기 지정 */}
             <div> 
-                <img src="kimleejung_logo.png"  className="borad-logo" alt='borad'/>
+                <img src="kimleejung_logo.png" className="borad-llogo" alt='borad'/>
             </div>
+            <div style={{  padding: "50px 12px" }}>
+               <table className="board_list">
+                  <colgroup>
+                     <col width="5%" />
+
+                     <col width="50%" />
+                     <col width="*" />
+                     <col width="*" />
+                     <col width="*" />
+                  </colgroup>
+                  <thead className="list">
+                     <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>작성자</th>
+                        <th>작성일시</th>
+                        <th>조회수</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+           
+          
+             
+                  </tbody>
+
+               </table>
+            </div>
+
+
             {/* 글쓰기 버튼 */}
             <form onSubmit={onPost}>
                 <button className="Post_Botton">
